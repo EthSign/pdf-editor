b138d9f41fc8351d94a5ebbad3903c55df429d29
miloliu-xyz <157335602+miloliu-xyz@users.noreply.github.com>
2024-08-16 17:29:15 +0800

    feat: add pdf-editor

---
+++ pdf-editor/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import { PDFEditor } from "./components/pdf-editor";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="w-screen h-screen">
+      <PDFEditor viewerUrl="/web/viewer.html" />
+    </div>
+  );
+}
+
+export default App;
